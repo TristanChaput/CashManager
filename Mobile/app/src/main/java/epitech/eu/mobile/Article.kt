08c62514c8608f93f6a1cd7756e2d28e57d7b3c8
Tristan <tristan.chaput@epitech.eu>
2020-11-27 17:03:53 +0100

    feat:liste des articles fonctionnel

--- Mobile/app/src/main/java/epitech/eu/mobile/Article.kt
+++ Mobile/app/src/main/java/epitech/eu/mobile/Article.kt
@@ -2,3 +2,8 @@ package epitech.eu.mobile
 
 class Article(val qrCode: String, val img: Int, val name: String, val description: String, val prix: Double) {
 }
+
+/*
+            val intent = Intent(this, Articles::class.java)
+            startActivity(intent)
+ */
--- Mobile/app/src/main/java/epitech/eu/mobile/Articles.kt
+++ Mobile/app/src/main/java/epitech/eu/mobile/Articles.kt
@@ -2,32 +2,28 @@ package epitech.eu.mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.Toast
 
 class Articles : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.articles)
-        val listView = findViewById<ListView>(R.id.articles_lv)
+        setContentView(R.layout.activity_articles)
 
-        val listArticles = ArrayList<Article>()
+        var listView = findViewById<ListView>(R.id.articles_lv)
+        var list = mutableListOf<Article>()
+        list.add(Article("123", R.drawable.croisiere, "Croisiere", "description voyage", 5.0))
 
-        for (i in 0..4) {
-            listArticles.add(Article("123", 0,"test", "description voyage",5.0))
-        }
         //A COMPLETER AVEC L'API
 
+        listView.adapter = ArticleAdapter(this, R.layout.row, list)
 
-        val adapter: ArrayAdapter<Article> =
-            ArrayAdapter(this, android.R.layout.simple_list_item_1, listArticles)
-        listView.adapter = adapter
-
-        listView.setOnItemClickListener { adapter, view, i, id ->
+        listView.setOnItemClickListener { parent: AdapterView<*>, view: View, position: Int, id: Long ->
             Toast.makeText(
-                this,
-                "Item Selected" + listArticles[i].name,
+                this@Articles,
+                "you select " + list.get(position).name + " travel",
                 Toast.LENGTH_LONG
             ).show()
         }
