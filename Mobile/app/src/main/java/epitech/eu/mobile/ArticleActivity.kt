b4371eec8bf50fbc54e2f74fd88c2f22edd978f3
Tristan <tristan.chaput@epitech.eu>
2020-12-10 18:42:46 +0100

    view articles

--- Mobile/app/src/main/java/epitech/eu/mobile/ArticleActivity.kt
+++ Mobile/app/src/main/java/epitech/eu/mobile/ArticleActivity.kt
@@ -1,8 +1,8 @@
 package epitech.eu.mobile
 
+import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -12,15 +12,10 @@ import androidx.recyclerview.widget.RecyclerView
 class ArticleActivity : AppCompatActivity(), ArticleAdapter.OnArticleClickListener {
     private val articleList = generateArticleList()
     private val adapter = ArticleAdapter(articleList, this)
-    private var delay = 0
-    private var info = false
-    lateinit var mainHandler: Handler
+    private val TAG: String = ArticleActivity::class.java.simpleName
 
-    private val updateDelayTask = object : Runnable {
-        override fun run() {
-            minusOneSecondDelay()
-            mainHandler.postDelayed(this, 1000)
-        }
+    companion object {
+        const val INTENT_PARCELABLE = "OBJECT_INTENT"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,56 +23,54 @@ class ArticleActivity : AppCompatActivity(), ArticleAdapter.OnArticleClickListen
         setContentView(R.layout.activity_articles)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view_articles)
-        recyclerView.adapter = this.adapter
+        recyclerView.adapter = ArticleAdapter(articleList, this)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
-        mainHandler = Handler(Looper.getMainLooper())
     }
 
     fun insertArticle(view: View) {
-        articleList.add(Article("123", R.drawable.croisiere, "Croisiere qrcode", "", 567.00))
-        adapter.notifyItemInserted(articleList.size - 1)
+        val index = articleList.lastIndex + 1
+        Log.d(TAG, "Insertion article $index")
+        articleList.add(index, Article("123", R.drawable.croisiere, "Croisiere qrcode", "", 567.00))
+        Log.d(TAG, "article: "+ articleList[index].name)
+        adapter.notifyItemInserted(index)
+
     }
 
     override fun onArticleClick(position: Int) {
-        if (delay <= 0) {
-            Toast.makeText(this, "Article n°" + (position + 1) + " clicked", Toast.LENGTH_SHORT)
-                .show()
-            delay += 5
-            info = false
-            onResume()
-        } else if (!info) {
-            Toast.makeText(this, "Wait $delay s between every click", Toast.LENGTH_SHORT).show()
-            info = true
-        }
+        Toast.makeText(this, "Article n°" + (position + 1) + " clicked", Toast.LENGTH_SHORT)
+            .show()
+        val intent = Intent(this, DetailActivity::class.java)
+        intent.putExtra(INTENT_PARCELABLE, articleList[position])
+        startActivity(intent)
     }
 
     private fun generateArticleList(): ArrayList<Article> {
         val listArticle = ArrayList<Article>()
 
         //A COMPLETER AVEC L'API
-        listArticle.add(Article("123", R.drawable.croisiere, "Croisiere maldives",     "", 4999.99))
-        listArticle.add(Article("123", R.drawable.croisiere, "Croisiere costa rica",   "", 6299.99))
-        listArticle.add(Article("123", R.drawable.croisiere, "Croisiere iles canaries","", 3999.99))
-        listArticle.add(Article("123", R.drawable.croisiere, "Croisiere grece",        "", 7899.99))
+        listArticle.add(Article("123", R.drawable.croisiere, "Croisiere maldives", "", 4999.99))
+        listArticle.add(Article("123", R.drawable.croisiere, "Croisiere costa rica", "", 6299.99))
+        listArticle.add(
+            Article(
+                "123",
+                R.drawable.croisiere,
+                "Croisiere iles canaries",
+                "",
+                3999.99
+            )
+        )
+        listArticle.add(Article("123", R.drawable.croisiere, "Croisiere grece", "", 7899.99))
+        listArticle.add(
+            Article(
+                "123",
+                R.drawable.croisiere,
+                "Croisiere grece bis",
+                "une description",
+                7899.99
+            )
+        )
 
         return listArticle
-    }
-
-    override fun onPause() {
-        super.onPause()
-        mainHandler.removeCallbacks(updateDelayTask)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mainHandler.post(updateDelayTask)
-    }
-
-    private fun minusOneSecondDelay() {
-        if (delay > 0)
-            delay--
-        else
-            onPause()
     }
 }
--- Mobile/app/src/main/java/epitech/eu/mobile/ArticleAdapter.kt
+++ Mobile/app/src/main/java/epitech/eu/mobile/ArticleAdapter.kt
@@ -7,7 +7,10 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ArticleAdapter(private val articleList: List<Article>, private val listener: OnArticleClickListener) :
+class ArticleAdapter(
+    private val articleList: List<Article>,
+    private val listener: OnArticleClickListener
+) :
     RecyclerView.Adapter<ArticleAdapter.ArticleViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
@@ -26,7 +29,8 @@ class ArticleAdapter(private val articleList: List<Article>, private val listene
 
     override fun getItemCount() = articleList.size
 
-    inner class ArticleViewHolder(articleView: View) : RecyclerView.ViewHolder(articleView), View.OnClickListener{
+    inner class ArticleViewHolder(articleView: View) : RecyclerView.ViewHolder(articleView),
+        View.OnClickListener {
         val imageView: ImageView = articleView.findViewById(R.id.image_view)
         val textViewTitle: TextView = articleView.findViewById(R.id.TextViewTitle)
         val textViewPrice: TextView = articleView.findViewById(R.id.TextViewPrice)
@@ -37,13 +41,13 @@ class ArticleAdapter(private val articleList: List<Article>, private val listene
 
         override fun onClick(v: View?) {
             val position = adapterPosition
-            if(position != RecyclerView.NO_POSITION) {
+            if (position != RecyclerView.NO_POSITION) {
                 listener.onArticleClick(position)
             }
         }
     }
 
-    interface OnArticleClickListener{
+    interface OnArticleClickListener {
         fun onArticleClick(position: Int)
     }
 }
