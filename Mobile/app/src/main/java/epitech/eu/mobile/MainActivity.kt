b2c6d092ae4c62d0b3c2375c84763c1412c57f5b
Maxime <maxime.pasquier@studylink.fr>
2020-12-12 12:12:36 +0100

    adding network location

--- Mobile/app/src/main/java/epitech/eu/mobile/MainActivity.kt
+++ Mobile/app/src/main/java/epitech/eu/mobile/MainActivity.kt
@@ -7,6 +7,8 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.GsonBuilder
+import com.google.zxing.integration.android.IntentIntegrator
+import com.journeyapps.barcodescanner.CaptureActivity
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.MediaType.Companion.toMediaType
@@ -25,12 +27,19 @@ class MainActivity : AppCompatActivity() {
         // get reference to all views
         val etUserName = findViewById<EditText>(R.id.et_user_name)
         val etPassword = findViewById<EditText>(R.id.et_password)
+        val etNetworkLocation = findViewById<EditText>(R.id.et_network_location)
         val btnSubmit = findViewById<Button>(R.id.submit)
+
+        val btnQrCode = findViewById<Button>(R.id.qrcode)
+        btnQrCode.setOnClickListener {
+            scanQRCode()
+        }
 
         // set on-click listener
         btnSubmit.setOnClickListener {
-            /*val userName = etUserName.text;
+            val userName = etUserName.text;
             val password = etPassword.text;
+            val networkLocation = etNetworkLocation.text;
 
             val url = "http://192.168.1.80:8080/auth/local"
 
@@ -76,9 +85,30 @@ class MainActivity : AppCompatActivity() {
                         startActivity(intent)
                     }
                 }
-            })*/
-            val intent = Intent(this@MainActivity, ArticleActivity::class.java)
-            startActivity(intent)
+            })
+            //val intent = Intent(this@MainActivity, ArticleActivity::class.java)
+            //startActivity(intent)
+        }
+    }
+
+    private fun scanQRCode(){
+        val integrator = IntentIntegrator(this).apply {
+            captureActivity = CaptureActivity::class.java
+            setOrientationLocked(false)
+            setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
+            setPrompt("")
+        }
+        integrator.initiateScan()
+    }
+
+    // Get the results:
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            else Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 }
